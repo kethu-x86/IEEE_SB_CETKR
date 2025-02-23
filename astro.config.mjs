abc676050b7d8e93e3ab6792ffec68e773050b15
Anukheth Sunil M <anukhethsunil@gmail.com>
2025-02-23 13:04:01 +0530

    Add basic Integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [
+    react(),
+    sitemap(),
+    icon(),
+    playformCompress()
+  ]
+});
